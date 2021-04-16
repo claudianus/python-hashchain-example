c10695215340685fd33346fef7a0f0c58f370afc
claudianus mamertus <claudianus@engineer.com>
2021-04-16 09:13:36 +0900

    added random seed generation

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import hashlib
+import secrets
 
 hashchain = []
 hashchainLength = 1000_0000
-previousHash = ""
+previousHash = secrets.token_hex(1024) # generate 1024bytes random seed
 for i in range(hashchainLength):
     previousHash = hashlib.sha256(previousHash.encode("utf-8")).hexdigest()
     # hashchain.append(previousHash)
